7a12a29e668c93ec47673aa9c17d3ca85a2fdd12
Mike Purcell <digitalprecision@gmail.com>
2013-11-21 02:31:16 +0000

    Performance improvements and added another type

--- lib/Hobis/Api/Date/Month.php
+++ lib/Hobis/Api/Date/Month.php
@@ -2,7 +2,8 @@
 
 class Hobis_Api_Date_Month
 {
-    const FORMAT_STRING_SHORT = 1;
-    const FORMAT_STRING_LONG  = 2;
-    const FORMAT_NUMERIC      = 3;
+    const FORMAT_STRING_SHORT   = 1;
+    const FORMAT_STRING_LONG    = 2;
+    const FORMAT_NUMERIC_SHORT  = 3;
+    const FORMAT_NUMERIC_LONG   = 4;
 }
--- lib/Hobis/Api/Date/Month/Package.php
+++ lib/Hobis/Api/Date/Month/Package.php
@@ -1,26 +1,61 @@
 <?php
 
 class Hobis_Api_Date_Month_Package
-{    
+{
     /**
-     * Container for months in long string format
+     * Container for months as ints
+     * 
+     * @var array 
+     */
+    protected static $monthsAsInts;
+    
+    /**
+     * Container for months as strings
      * 
      * @var array
      */
-    protected static $months = array(
-        'January',
-        'Febuary',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December'
-    );
+    protected static $monthsAsStrings;
+    
+    /**
+     * Wrapper method for getting months as ints
+     * 
+     * @return array
+     */
+    protected static function getMonthsAsInts()
+    {
+        if (false === Hobis_Api_Array_Package::populated(self::$monthsAsInts)) {
+            self::$monthsAsInts = range(1, 12);
+        }
+        
+        return self::$monthsAsInts;
+    }
+    
+    /**
+     * Wrapper method for getting months as strings
+     * 
+     * @return array
+     */
+    protected static function getMonthsAsStrings()
+    {
+        if (false === Hobis_Api_Array_Package::populated(self::$monthsAsStrings)) {
+            self::$monthsAsStrings = array(
+                'January',
+                'Febuary',
+                'March',
+                'April',
+                'May',
+                'June',
+                'July',
+                'August',
+                'September',
+                'October',
+                'November',
+                'December'
+            );
+        }
+        
+        return self::$monthsAsStrings;
+    }
     
     /**
      * Wrapper method for creating an array of months based on format
@@ -28,22 +63,26 @@ class Hobis_Api_Date_Month_Package
      * @param string
      * @return array
      */
-    public static function toArray($format = Hobis_Api_Date_Month::FORMAT_NUMERIC)
+    public static function toArray($format = Hobis_Api_Date_Month::FORMAT_NUMERIC_SHORT)
     {
         switch ($format) {
         
             case Hobis_Api_Date_Month::FORMAT_STRING_LONG:
-                $months = self::$months;
+                $months = self::getMonthsAsStrings();
                 break;
             
             case Hobis_Api_Date_Month::FORMAT_STRING_SHORT:
-                $months = array_map(function ($element) { return substr($element, 0, 3); }, self::$months);
+                $months = array_map(function ($element) { return substr($element, 0, 3); }, self::getMonthsAsStrings());
+                break;
+            
+            case Hobis_Api_Date_Month::FORMAT_NUMERIC_LONG:
+                $months = array_map(function ($element) { return (strlen($element) < 2) ? sprintf('0%s', $element) : $element; }, self::getMonthsAsInts());
                 break;
             
             default:
-                $months = range(1, 12);
+                $months = self::getMonthsAsInts();
         }
         
-        return array_combine(range(1, 12), $months);
+        return array_combine(self::getMonthsAsInts(), $months);
     }
 }
