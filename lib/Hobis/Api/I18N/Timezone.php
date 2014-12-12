aef45947a23a828ef8648c29385e0121dc598fc8
Mike Purcell <digitalprecision@gmail.com>
2014-12-12 22:46:07 +0000

    OPZ-12: Adding low level libs to support app feature

---
+++ lib/Hobis/Api/I18N/Timezone.php
@@ -0,0 +1,7 @@
+<?php
+
+class Hobis_Api_I18N_Timezone
+{
+    const AMERICA_LOS_ANGELES   = 'america/los_angeles';
+    const UTC                   = 'utc';
+}
---
+++ lib/Hobis/Api/Interval.php
@@ -0,0 +1,12 @@
+<?php
+
+class Hobis_Api_Interval
+{
+    const SECONDLY  = 1;
+    const MINUTELY  = 2;
+    const HOURLY    = 3;
+    const DAILY     = 4;
+    const WEEKLY    = 5;
+    const MONTHLY   = 6;
+    const YEARLY    = 7;
+}
