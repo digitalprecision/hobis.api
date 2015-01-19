e315211c937c1420cf88b278eb6290f3d1d97aac
Mike Purcell <digitalprecision@gmail.com>
2015-01-19 02:05:10 +0000

    Adding wrapper methods for determing if server is prod or dev

--- lib/Hobis/Api/Environment/Server/Package.php
+++ lib/Hobis/Api/Environment/Server/Package.php
@@ -11,4 +11,24 @@ class Hobis_Api_Environment_Server_Package
     {
         return (bool) (PHP_SAPI === 'cli');
     }
+
+    /**
+     * Wrapper method for determining if server is flagged as dev
+     *
+     * @return bool
+     */
+    public static function isDev()
+    {
+        return (Hobis_Api_Environment_Package::getValue(Hobis_Api_Environment::VAR_LABEL_SERVICE) === Hobis_Api_Environment::DEV);
+    }
+
+    /**
+     * Wrapper method for determining if server is flagged as prod
+     *
+     * @return bool
+     */
+    public static function isProd()
+    {
+        return (Hobis_Api_Environment_Package::getValue(Hobis_Api_Environment::VAR_LABEL_SERVICE) === Hobis_Api_Environment::PROD);
+    }
 }
